7ee425e7adff6f4b773416a4af380b3d2b0b8f5e
Ahmed <kadic.ahmed@edu.fit.ba>
2022-06-09 14:54:43 +0200

    BUG fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,9 @@ import { TimComponent } from './components/tim/tim.component';
 import { TimEditComponent } from './components/tim-edit/tim-edit.component';
 import {HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatDividerModule} from "@angular/material/divider";
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MainviewComponent } from './components/mainview/mainview.component';
+
 
 @NgModule({
   declarations: [
@@ -34,11 +30,6 @@ import { MainviewComponent } from './components/mainview/mainview.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
